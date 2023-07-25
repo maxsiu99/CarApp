70b7a1e88e03315dbedf3cee91151d9025731055
maxsiu99 <maxsiu99@gmail.com>
2023-07-25 12:07:47 +0200

    pierwszy comit

---
+++ app/src/main/java/TankActivity.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.carapp.R;
+
+public class TankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tank_lasyout);
+    }
+}
